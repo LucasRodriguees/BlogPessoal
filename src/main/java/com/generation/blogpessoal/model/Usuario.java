360fa233a74316049a6aaa8711b75bd57ba8a089
Lucas <lucas.orodrigues@hotmail.com>
2022-03-10 15:07:10 -0300

    aplicaçãocompleta

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -27,6 +29,7 @@ public class Usuario {
 	@NotNull(message = "o atributo Nome é obrigatorio!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "o atributo Usuário é obrigatorio! ")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
